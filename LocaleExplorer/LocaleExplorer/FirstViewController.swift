108311433115c755f2ad600e62774262c443245e
Kaho Lo <kahol@adobe.com>
2015-05-14 00:47:27 -0700

    Initial project files.

---
+++ LocaleExplorer/LocaleExplorer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LocaleExplorer
+//
+//  Created by Kaho Lo on 5/14/15.
+//  Copyright (c) 2015 Globalization. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LocaleExplorer/LocaleExplorer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LocaleExplorer
+//
+//  Created by Kaho Lo on 5/14/15.
+//  Copyright (c) 2015 Globalization. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
